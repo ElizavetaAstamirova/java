79738c61fd53e2ec9c42e1a726ebcc7a6d08ff13
ElizavetaAstamirova <92157888+ElizavetaAstamirova@users.noreply.github.com>
2024-04-14 12:30:45 +0300

    Delete projectGit directory

--- projectGit/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projectgit {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projectgit to javafx.fxml;
-    exports com.example.projectgit;
-}
